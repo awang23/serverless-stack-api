b4a63fe905c8872f5ecd6d6a87249d11278566f5
awang23 <awang23@stanford.edu>
2021-06-28 22:54:26 -0500

    Adding CORS to our Serverless API

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -10,9 +10,13 @@ export default function handler(lambda) {
         statusCode = 500;
       }
     // Return HTTP response
-      return {
+    return {
         statusCode,
         body: JSON.stringify(body),
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
       };
     };
   }
